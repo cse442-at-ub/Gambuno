eaea88959b39c9cf3ddf90b1d7cf3c5d0fce9af9
kurianv <86732303+kurianv@users.noreply.github.com>
2025-02-14 22:14:40 -0500

    got to deploy on cattle, but only a white html screen

--- unogame/src/App.jsx
+++ unogame/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
-import MainMenu from './components/MainMenu.tsx';
-import PlayGame from './components/PlayGameMenu.tsx';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
+import MainMenu from './components/MainMenu.jsx';
+import PlayGame from './components/PlayGameMenu.jsx';
 
 function App() {
   return (

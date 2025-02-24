045efcc867412722f77d549cff66eb6fc6a1287f
haoyangan1 <haoyanga@buffalo.edu>
2025-02-24 11:13:50 -0500

    Add Friend Features
    Combine Friend Features into main menu

--- unogame/src/App.jsx
+++ unogame/src/App.jsx
@@ -4,12 +4,26 @@ import MainMenu from './components/MainMenu';
 import PlayGame from './components/PlayGameMenu';
 import TempPath from './components/TempPath';
 import Tutorial from "./components/Tutorial"
+import FriendList from './components/FriendList/FriendList';
+
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainMenu />} />
+        {/* Main Menu with Friend List */}
+        <Route 
+          path="/" 
+          element={
+            <div className="main-menu-container">
+              <MainMenu />
+              <FriendList /> {/* Add FriendList here */}
+            </div>
+          } 
+        />
+        
+        {/* Other routes without Friend List */}
         <Route path="/play" element={<PlayGame />} />
         <Route path="/card" element={<TempPath />} />
         <Route path="/tutorial" element={<Tutorial />} />
@@ -19,3 +33,7 @@ function App() {
 }
 
 export default App;
+
+
+
+

b6ae8a357f4d113a73f39a03d96542c2d8e42c37
kurianv <86732303+kurianv@users.noreply.github.com>
2025-03-01 11:55:09 -0500

    merged-friends-list-and-game-logic

--- unogame/src/App.jsx
+++ unogame/src/App.jsx
@@ -3,7 +3,6 @@ import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import MainMenu from './components/MainMenu';
 import PlayGame from './components/PlayGameMenu';
 import TempPath from './components/TempPath';
-
 import Tutorial from "./components/Tutorial";
 import LogIn from './components/LogIn';
 import CreateAccount from './components/CreateAccount'; 
@@ -13,14 +12,21 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route 
+          path="/" 
+          element={
+            <div className="main-menu-container">
+              <MainMenu />
+              <FriendList /> {/* Add FriendList here */}
+            </div>
+          } 
+        />
         {/* Other routes without Friend List */}
         <Route path="/play" element={<PlayGame />} />
         <Route path="/card" element={<TempPath />} />
         <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/create-account" element={<CreateAccount />} />
-
-        <Route path="/" element={<MainMenu />} />
         <Route path="/play" element={<PlayGame />} />
 
       </Routes>

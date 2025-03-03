dbbc9e28d1e9c55ad63217774136528a37814cee
kurianv <86732303+kurianv@users.noreply.github.com>
2025-03-02 19:51:18 -0500

    No user story because just connecting the pages for traversal

--- unogame/src/components/MainMenu.jsx
+++ unogame/src/components/MainMenu.jsx
@@ -21,6 +21,11 @@ function MainMenu() {
   
 };
 
+const goToTutorial = () => {
+  navigate("/tutorial");
+
+};
+
   return (
     <div className={styles.MainMenu_1_2}>
       <div className={styles.Ellipse_2_76_55}></div><span className={styles.NextGen_14_4}>NEXT GEN </span><span className={styles.Uno_76_50}>UNO </span>
@@ -44,9 +49,6 @@ function MainMenu() {
         </svg>
       </div>
       <div className={styles.PlayButton_80_77}><svg width="220" height="100" viewBox="0 0 356 124" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-        
-
           <g filter="url(#filter0_d_80_77)">
             <rect width="355.27" height="124" rx="60" fill="#F42C04" fillOpacity="0.95" />
             <rect x="2.5" y="2.5" width="350.27" height="119" rx="57.5" stroke="#FFB30F" stroke-opacity="0.5" strokeWidth="5" />
@@ -65,6 +67,11 @@ function MainMenu() {
           </defs>
         </svg>
       </div>
+      <div className={styles.HelpTutorialCircle_154_45} onClick={goToTutorial}><svg width="65" height="66" viewBox="0 0 65 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M24.6186 24.75C25.2553 22.9121 26.5121 21.3623 28.1664 20.3751C29.8206 19.3879 31.7656 19.0271 33.6568 19.3565C35.548 19.6858 37.2633 20.6842 38.499 22.1747C39.7347 23.6652 40.411 25.5517 40.4082 27.5C40.4082 33 32.2832 35.75 32.2832 35.75M32.4998 46.75H32.5269M59.5832 33C59.5832 48.1878 47.4575 60.5 32.4998 60.5C17.5421 60.5 5.4165 48.1878 5.4165 33C5.4165 17.8122 17.5421 5.5 32.4998 5.5C47.4575 5.5 59.5832 17.8122 59.5832 33Z" stroke="#1E1E1E" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+      </div>
+
       <div className={styles.CreditsButton_14_7}></div><span className={styles.Credits_26_17}><button className={styles.Credits_26_17} onClick={goToCard}>Credits</button></span>
       <div className={styles.LoginButton_14_8}></div><span><button className={styles.LogIn_26_10} onClick={goToLogin}> Log In </button></span><span><button className={styles.Play_80_74} onClick={goToPlayPage}>Play</button></span><span className={styles.Settings_80_79}>Settings</span>
     </div>

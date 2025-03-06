7c4e5e0e8a05913dc88ddb959227fb12b691f706
kurianv <86732303+kurianv@users.noreply.github.com>
2025-03-05 22:39:44 -0500

    tutorial appears as a pop out instead of a page

--- unogame/src/components/MainMenu.jsx
+++ unogame/src/components/MainMenu.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/MainMenu.module.css';
 import { useNavigate } from 'react-router-dom';
-
+import Tutorial from './Tutorial';
 
 function MainMenu() {
     const navigate = useNavigate();
-
+    const [isTutOpen, setIsTutOpen] = useState(false)
     const goToPlayPage = () => {
       navigate("/play");
 
@@ -21,10 +21,6 @@ function MainMenu() {
   
 };
 
-const goToTutorial = () => {
-  navigate("/tutorial");
-
-};
 
   return (
     <div className={styles.MainMenu_1_2}>
@@ -67,12 +63,13 @@ const goToTutorial = () => {
           </defs>
         </svg>
       </div>
-      <div className={styles.HelpTutorialCircle_154_45} onClick={goToTutorial}><svg width="65" height="66" viewBox="0 0 65 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <div className={styles.HelpTutorialCircle_154_45} onClick={() => setIsTutOpen(true)}><svg width="65" height="66" viewBox="0 0 65 66" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M24.6186 24.75C25.2553 22.9121 26.5121 21.3623 28.1664 20.3751C29.8206 19.3879 31.7656 19.0271 33.6568 19.3565C35.548 19.6858 37.2633 20.6842 38.499 22.1747C39.7347 23.6652 40.411 25.5517 40.4082 27.5C40.4082 33 32.2832 35.75 32.2832 35.75M32.4998 46.75H32.5269M59.5832 33C59.5832 48.1878 47.4575 60.5 32.4998 60.5C17.5421 60.5 5.4165 48.1878 5.4165 33C5.4165 17.8122 17.5421 5.5 32.4998 5.5C47.4575 5.5 59.5832 17.8122 59.5832 33Z" stroke="#1E1E1E" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
       </div>
+      <Tutorial isOpen ={isTutOpen} onClose={()=> setIsTutOpen(false)}/>
 
-      <div className={styles.CreditsButton_14_7}></div><span className={styles.Credits_26_17}><button className={styles.Credits_26_17} onClick={goToCard}>Credits</button></span>
+      <div className={styles.CreditsButton_14_7}></div><span className={styles.Credits_26_17}><button className={styles.Credits_26_17}>Credits</button></span>
       <div className={styles.LoginButton_14_8}></div><span><button className={styles.LogIn_26_10} onClick={goToLogin}> Log In </button></span><span><button className={styles.Play_80_74} onClick={goToPlayPage}>Play</button></span><span className={styles.Settings_80_79}>Settings</span>
     </div>
   );

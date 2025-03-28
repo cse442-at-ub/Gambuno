1cd17baed827b2ef2192aba65417cacf8c8ba5b6
kurianv <86732303+kurianv@users.noreply.github.com>
2025-03-28 13:43:48 -0400

    money is display properly

--- unogame/src/components/MainMenu.jsx
+++ unogame/src/components/MainMenu.jsx
@@ -166,7 +166,7 @@ useEffect(() => {
           <>
             <span className={`${styles.username_display} ${styles.LogIn_26_10}`}>{username}</span>
             <span className={styles.money_display}>
-              Money: ${!isLoading ? money : 'Loading...'}
+              ${!isLoading ? money : 'Loading...'}
             </span>
           </>
         ) : (

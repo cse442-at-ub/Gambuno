2ed4a9fdadd5231be7ab120ed8406cb2af8910ea
kurianv <86732303+kurianv@users.noreply.github.com>
2025-03-27 21:19:22 -0400

    updated db

--- unogame/api/database/cse442_2025_spring_team_c_db (2).sql
+++ unogame/api/database/cse442_2025_spring_team_c_db (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 27, 2025 at 09:15 PM
+-- Generation Time: Mar 27, 2025 at 09:18 PM
 -- Server version: 8.0.39-0ubuntu0.22.04.1
 -- PHP Version: 8.1.2-1ubuntu2.18
 

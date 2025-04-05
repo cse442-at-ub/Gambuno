39799a7564b7278d24560dd26c8cb8b04cad5420
haoyangan1 <haoyanga@buffalo.edu>
2025-04-04 23:15:58 -0400

    Update WaitingRoomHostD.jsx

--- unogame/src/components/WaitingRoom/WaitingRoomHostD.jsx
+++ unogame/src/components/WaitingRoom/WaitingRoomHostD.jsx
@@ -180,6 +180,13 @@ const WaitingRoomHost = () => {
           <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v2h-2zm0 4h2v6h-2z" />
         </svg>
       </button>
+
+      <button
+          onClick={() => navigate("/uno-game")}
+          className="mt-6 px-6 py-3 bg-white text-blue-500 text-xl font-bold shadow-lg rounded-xl border-2 border-blue-500 hover:bg-blue-500 hover:text-white transition"
+        >
+          Start
+        </button>
     </div>
   );
 };

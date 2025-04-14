a6327733dfd7fd8cecb3c59c885fbe0ad2eeda03
codyyam <codyyam@buffalo.edu>
2025-04-14 01:19:17 -0400

    modified gameboard to redirect the host to the hostgamelobby and all other players to the waitinglobby when someone wins an UNO game and the game is finished

--- unogame/src/components/gameLayout/game-board.jsx
+++ unogame/src/components/gameLayout/game-board.jsx
@@ -348,7 +348,12 @@ export default function UnoGameBoard() {
       if (state.success) {
         setGameState(state)
         if (String(state.gameStatus) === "finished") {
-          navigate("/");
+          if (state.host === playerID) {
+            navigate(`/host-game-lobby/${gameID}`);
+          } else {
+            navigate(`/waiting-host/${gameID}`);
+          }
+          return;
         }
 
         // Find the current player's data to get their hand

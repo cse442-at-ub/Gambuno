540dbff7e23bf7147096cf9530c2fb10ddee4379
rancescolon <109361303+rancescolon@users.noreply.github.com>
2025-04-17 18:18:43 -0400

    fixed xxs stuff

--- unogame/api/index.php
+++ unogame/api/index.php
@@ -1,7 +1,19 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+$allowed_origins = [
+    'https://se-dev.cse.buffalo.edu/CSE442/2025-Spring/cse-442c/api/',
+    'https://se-dev.cse.buffalo.edu/CSE442/2025-Spring/cse-442c/api/'
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (in_array($origin, $allowed_origins, true)) {
+    header('Access-Control-Allow-Origin: ' . $origin);
+    header('Access-Control-Allow-Credentials: true');
+}
+
 
 require_once 'auth.php';
 
